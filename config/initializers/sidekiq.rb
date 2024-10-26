9f21d0318560d4e00209b2cf3b161cb6f258ccb4
kmstheodore <108134766+kmstheodore@users.noreply.github.com>
2024-10-26 22:52:49 +0100

    fiddle with sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://red-cse6odu8ii6s738tohf0:6379" }
+  config.redis = { url: "redis://:R59dO3BWIjeZg5QKiGwuHI0MmXDOSbLl@oregon-redis.render.com:6379", ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://red-cse6odu8ii6s738tohf0:6379" }
+  config.redis = { url: "redis://:R59dO3BWIjeZg5QKiGwuHI0MmXDOSbLl@oregon-redis.render.com:6379", ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
