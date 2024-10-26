b0d2fc1b3811b4ed28107efdc920c1bfa98403cc
kmstheodore <108134766+kmstheodore@users.noreply.github.com>
2024-10-26 16:54:23 +0100

    add routes for event actions new and index

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root to: "pages#home"
+  get "events/new", to: "events#new"
+  get "events/index", to: "events#index"
 end
